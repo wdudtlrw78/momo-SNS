611094b24cafd84ebbad06417cc84a0e3aa9e42e
momo <wdudtlrw78@gmail.com>
2021-07-15 16:01:55 +0900

    Add: signin page

---
+++ pages/signin.jsx
@@ -0,0 +1,84 @@
+import React, { useCallback } from 'react';
+import Link from 'next/link';
+import { Form, Input, Button, Checkbox } from 'antd';
+import styled from '@emotion/styled';
+import { useDispatch } from 'react-redux';
+import Router from 'next/router';
+import AppLayout from '../components/AppLayout';
+import { loginAction } from '../reducers';
+
+const SignGroup = styled.div`
+  @media (max-width: 575px) {
+    max-width: 90%;
+  }
+`;
+
+function SignIn() {
+  const dispatch = useDispatch();
+
+  const onSubmitForm = useCallback((values) => {
+    dispatch(loginAction(values));
+
+    if (values) {
+      Router.replace('/');
+    }
+  }, []);
+
+  return (
+    <AppLayout>
+      <div
+        style={{
+          fontFamily: 'Roboto',
+          height: '60vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '0 16px',
+        }}
+      >
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onSubmitForm}
+          style={{ width: '364px' }}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit" loading={false}>
+              Next
+            </Button>
+          </Form.Item>
+          <SignGroup style={{ width: '120%', textAlign: 'center' }}>
+            Don&apos;t have an account?
+            <Link href="/signup">
+              <a style={{ marginLeft: '4px', color: '#008CFF' }}>Sign Up</a>
+            </Link>
+          </SignGroup>
+        </Form>
+      </div>
+    </AppLayout>
+  );
+}
+
+export default SignIn;
