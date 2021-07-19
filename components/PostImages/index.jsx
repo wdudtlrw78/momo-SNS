111c78d413dccf78093f053e5585bfbb95ee71c1
momo <wdudtlrw78@gmail.com>
2021-07-19 20:06:57 +0900

    Add: PostImages Component

--- components/PostImages/index.jsx
+++ components/PostImages/index.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
 
 function PostImages({ images }) {
-  return <div>image</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  });
+
+  if (images.length === 1) {
+    return (
+      <>
+        {/* role: 시각장애인분들을 위해 스크린 리더에서 굳이 얘를 클릭 할 필요있다 여부를 안알려줄때만 넣어준다. */}
+        <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+      </>
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        {/* role: 시각장애인분들을 위해 스크린 리더에서 굳이 얘를 클릭 할 필요있다 여부를 안알려줄때만 넣어준다. */}
+        <img
+          role="presentation"
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+  return (
+    <>
+      <img
+        role="presentation"
+        style={{ width: '50%', display: 'inline-block' }}
+        src={images[0].src}
+        alt={images[0].src}
+      />
+      <div
+        role="presentation"
+        style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+        onClick={onZoom}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1}
+        개의 사진 더보기
+      </div>
+    </>
+  );
 }
 PostImages.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.object),
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 export default PostImages;
